b1b2cadf70799e6b7564f388bf681175a1729afb
turek871 <104063732+turek871@users.noreply.github.com>
2022-04-20 10:45:55 +0200

    Add files via upload

---
+++ copySP.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 16:52:25 2022
+
+@author: aturowski
+"""
+
+import shutil
+
+shutil.copyfile('test.txt', '//sharepoint.polcard.com.pl/psd/ZWS/02_PLAN_HANDLOWY/2021_BDGT/00_NS_AT/test.txt')
+#shutil.copyfile('test.txt', 'copy.txt')
+#shutil.copyfile('E:/ZWS/Artur Turowski/Przeglądarka/2017/1-01_012022.accdb', '//sharepoint.polcard.com.pl/psd/ZWS/02_PLAN_HANDLOWY/2021_BDGT/00_NS_AT/1-01_012022.accdb')
+
+
